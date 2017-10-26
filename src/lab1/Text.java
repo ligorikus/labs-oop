68a765894a0f6b42e052bf441fd0649bf9b3d267
semal55 <semal551@gmail.com>
2017-10-26 08:58:14 +0600

    Create First JUnit Tests

---
+++ src/lab1/Text.java
@@ -0,0 +1,24 @@
+package lab1;
+
+public class Text {
+	
+	private String text;
+	
+	public String getText() {
+		return text;
+	}
+
+	public void setText(String text) {
+		this.text = text;
+	}
+
+	public void init(String text)
+	{
+		this.text = text;
+	}
+	
+	public void clear()
+	{
+		//TODO: clear this.text
+	}
+}
--- src/tests/Lab1Tests.java
+++ src/tests/Lab1Tests.java
@@ -2,10 +2,26 @@ package tests;
 
 import static org.junit.Assert.*;
 
-import org.junit.Test;
+import org.junit.*;
+
+import lab1.Text;
 
 public class Lab1Tests {
 
+	private Text text;
+	
+	@Before
+	public void init() 
+	{
+		text = new Text();
+	}
+	
+	@After
+	public void tearDown()
+	{
+		text = null;
+	}
+	
 	@Test
 	public void test() {
 		fail("Not yet implemented");
