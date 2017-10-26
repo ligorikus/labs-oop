5e5abe781040248cc027d9c9c52154c5a07c20c0
semal55 <semal551@gmail.com>
2017-10-26 09:27:37 +0600

    Finish writing tests

--- src/tests/Lab1Tests.java
+++ src/tests/Lab1Tests.java
@@ -2,29 +2,42 @@ package tests;
 
 import static org.junit.Assert.*;
 
+import java.util.*;
+
 import org.junit.*;
+import org.junit.runner.RunWith;
+import org.junit.runners.*;
+import org.junit.runners.Parameterized.*;
 
 import lab1.Text;
 
+@RunWith(Parameterized.class)
 public class Lab1Tests {
-
-	private Text text;
 	
-	@Before
-	public void init() 
+	@Parameters
+	public static Collection<Object[]> data()
 	{
-		text = new Text();
+		return Arrays.asList(new Object[][]{
+			{"a1b2c3d 4efg/6", "abcd efg"},
+			{"sf.12o55je///os 99sdf", "sfojeos sdf"}
+		});
 	}
 	
-	@After
-	public void tearDown()
+	private String init;
+	private String result;
+	
+	public Lab1Tests(String init, String result)
 	{
-		text = null;
+		this.init = init;
+		this.result = result;
 	}
 	
 	@Test
 	public void test() {
-		fail("Not yet implemented");
+		Text text = new Text();
+		text.init(this.init);
+		text.clear();
+		assertEquals(this.result, text.getText());
 	}
 
 }
